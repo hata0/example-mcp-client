bd8915b20ca1891a43096889bbd17b0f5d02b531
hata0 <haataa0100@gmail.com>
2025-07-07 11:51:34 +0900

    feat: mcp client追加

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,37 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const client = new Client({
+  name: "example-mcp-client",
+  version: "1.0.0",
+});
+
+const transport = new StreamableHTTPClientTransport(
+  new URL("https://example-mcp.haataa0100.workers.dev/mcp")
+);
+
+app.get("/chat", async (c) => {
+  await client.connect(transport);
+
+  const tools = await client.listTools();
+
+  return c.json({ tools });
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
